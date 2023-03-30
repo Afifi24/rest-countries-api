6b4ab2cb1ef95da4acd784f3b15686228a52c7a5
afifi brahim <afifi71brahim@gmail.com>
2023-03-30 16:50:57 +0000

    add filter functionality

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,30 +1,68 @@
-import React, { useEffect,useState } from 'react'
+import React, { useEffect,useState,useRef } from 'react'
 import CountriesItems from './CountriesItems'
 import Search from './Search'
 const Countries = ({theme,AddtoDetails}) => {
   const [Data,setData] = useState()
+  const [status,setStatus] = useState('')
+  const [region,setRegion] = useState('Filter by Region')
+  const [desepear,setDesepear] = useState(false)
   const [filteredbyletter,setFilteredbyletter] =useState()
- 
+  const [input,setInput] = useState('')
+  const [isopen,setIsopen]= useState(false)
+
   const FetchData = async()=>{
       const data = await fetch('https://restcountries.com/v3.1/all')
       const response = await data.json()
       setData(response)
   }
-  const FilteredFunction = ()=>{
-    if(Data){
-      const FilteredItem = Data.filter(filte=>filte.capital.toLowercase())
-      console.log(FilteredItem)
-    }
+  const FetchDataAfrica = async()=>{
+      const data = await fetch('https://restcountries.com/v3.1/region/africa')
+      const response = await data.json()
+      setData(response)
   }
+  const FetchDataAsia = async()=>{
+      const data = await fetch('https://restcountries.com/v3.1/region/asia')
+      const response = await data.json()
+      setData(response)
+  }
+  const FetchDataEurope = async()=>{
+      const data = await fetch('https://restcountries.com/v3.1/region/europe')
+      const response = await data.json()
+      setData(response)
+  }
+  const FetchDataAmericas = async()=>{
+      const data = await fetch('https://restcountries.com/v3.1/region/americas')
+      const response = await data.json()
+      setData(response)
+  }
+  const FetchDataOceania = async()=>{
+      const data = await fetch('https://restcountries.com/v3.1/region/oceania')
+      const response = await data.json()
+      setData(response)
+  }
+  const Delete = ()=>{
+    setRegion('Filter by Region')
+    setDesepear(false)
+  }
+  const reff = useRef()
+  const CloseFilter = (e)=>{
+       if(reff.current== e.target)
+       setIsopen(!isopen)
+  }
+  
   useEffect(()=>{
     FetchData()
-    FilteredFunction()
-  },[])
+  },[input])
   return (
-    <div className='py-20 px-8 md:px-14'>
-      <Search theme={theme}/>
+    <div onClick={CloseFilter} ref={reff} className='py-20 px-8 md:px-14'>
+      <Search FetchDataOceania={FetchDataOceania} FetchDataAmericas={FetchDataAmericas} FetchDataEurope={FetchDataEurope} FetchDataAsia={FetchDataAsia} FetchDataAfrica={FetchDataAfrica} reff={reff} isopen={isopen} setIsopen={setIsopen} setDesepear={setDesepear} desepear={desepear} Delete={Delete} region={region} setRegion={setRegion} setInput={setInput} input={input} theme={theme}/>
      <div className='grid grid-cols-1 font-NunitoSans  md:grid-cols-2 lg:grid-cols-3  gap-y-16 gap-x-14'>
-         {Data && Data.map(item=>(
+         {Data && Data.filter(filt=>{
+          if(input=='')
+          return filt
+          else if(filt.name.common.toLocaleLowerCase().includes(input.toLocaleLowerCase()))
+          return filt
+         }).map(item=>(
               <CountriesItems AddtoDetails={AddtoDetails} theme={theme} key={item.flags.png} item={item}/>
          ))}
      </div>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,30 +1,67 @@
 import React, { useState } from 'react'
 import {BiSearch} from 'react-icons/bi'
+import {FaRegTimesCircle} from 'react-icons/fa'
 import {IoIosArrowDown} from 'react-icons/io'
-const Search = ({theme}) => {
-    const [isopen,setIsopen]= useState(false)
+const Search = ({theme,setInput,input,setRegion,region,Delete,desepear,setDesepear,setIsopen,isopen,reff,FetchDataAfrica,FetchDataAsia,FetchDataEurope,FetchDataAmericas,FetchDataOceania}) => {
     const OpenClose = ()=>{
         setIsopen(!isopen)
+    }
+    const Africa = ()=>{
+        setRegion('Africa')
+        setDesepear(true)
+        setIsopen(false)
+        FetchDataAfrica()
+    }
+    const Asia = ()=>{
+        setRegion('Asia')
+        setDesepear(true)
+        setIsopen(false)
+        FetchDataAsia()
+
+    }
+    const America = ()=>{
+        setRegion('America')
+        setDesepear(true)
+        setIsopen(false)
+        FetchDataAmericas()
+
+    }
+    const Europe = ()=>{
+        setRegion('Europe')
+        setDesepear(true)
+        setIsopen(false)
+        FetchDataEurope()
+
+    }
+    const Oceania = ()=>{
+        setRegion('Oceania')
+        setDesepear(true)
+        setIsopen(false)
+        FetchDataOceania()
+
     }
   return (
     <div className='flex gap-6 md:gap-0  md:items-center justify-between mb-10 flex-col sm:flex-row'>
         <div className={`  w-1/2 relative`}>
-            <input className={` ${theme ? 'btn':''} py-5 text-sm md:text-md  outline-none rounded-md shadoww px-14 w-[250px] sm:w-[90%]  md:w-[90%] `} type="text" placeholder='Search for a country...' />
-            <BiSearch className={` ${theme ? 'text-white':''} absolute text-2xl  left-4 top-1/2 -translate-y-1/2`}/>
+            <input value={input} onChange={(e)=>setInput(e.target.value)} className={` ${theme ? 'btn':''} py-5 text-sm md:text-md  outline-none rounded-md shadoww px-14 w-[250px] sm:w-[90%]  md:w-[90%] `} type="text" placeholder='Search for a country...' />
+            <BiSearch className={` ${theme ? 'text-white':''} duration-200 absolute text-2xl  left-4 top-1/2 -translate-y-1/2`}/>
         </div>
         <div className='  relative '>
-            <div onClick={OpenClose} className={`${theme ? 'btn':''} bg-white flex justify-between items-center w-[200px] shadoww rounded-md cursor-pointer text-center px-4 py-5`}>
-            <p>filter by region</p>
-             <IoIosArrowDown/>
+            <div  className={`${theme ? 'btn':''} duration-200 bg-white flex justify-between items-center w-[200px] relative shadoww rounded-md cursor-pointer text-center px-4 py-5`}>
+            <p onClick={OpenClose}>{region}</p>
+             <IoIosArrowDown onClick={OpenClose}/>
+              { desepear &&
+                <FaRegTimesCircle onClick={Delete} className='absolute z-10 right-12'/>
+              }
             </div>
-            { isopen &&
-                <div className={` ${theme ? 'btn':''} bg-white absolute mt-2 flex flex-col   w-[200px] shadoww rounded-md cursor-pointer text-start  py-5`}>
-                <p className='px-4 py-1 hover:bg-gray-200 duration-200'>Africa</p>
-                <p className='px-4 py-1 hover:bg-gray-200 duration-200'>Asia</p>
-                <p className='px-4 py-1 hover:bg-gray-200 duration-200'>Europe</p>
-                <p className='px-4 py-1 hover:bg-gray-200 duration-200'>America</p>
-                <p className='px-4 py-1 hover:bg-gray-200 duration-200'>Oceania</p>
-            </div>
+            { isopen ?
+                <div ref={reff} className={` ${theme ? 'btn':''} duration-200 bg-white absolute mt-2 flex flex-col   w-[200px] shadoww rounded-md cursor-pointer text-start  py-5`}>
+                <p onClick={Africa} className={`${theme ? 'hover:bg-gray-800':''} px-4 py-1 hover:bg-gray-200 duration-200`}>Africa</p>
+                <p onClick={Asia} className={`px-4 py-1 ${theme ? 'hover:bg-gray-800':''} hover:bg-gray-200 duration-200`}>Asia</p>
+                <p onClick={Europe} className={`px-4 py-1 ${theme ? 'hover:bg-gray-800':''} hover:bg-gray-200 duration-200`}>Europe</p>
+                <p onClick={America} className={`px-4 py-1 ${theme ? 'hover:bg-gray-800':''} hover:bg-gray-200 duration-200`}>America</p>
+                <p onClick={Oceania} className={`px-4 py-1 ${theme ? 'hover:bg-gray-800':''} hover:bg-gray-200 duration-200`}>Oceania</p>
+            </div>:null
             }
         </div>
     </div>
